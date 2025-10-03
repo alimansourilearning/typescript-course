728fe37f480c5cae0bad4f576946ac4ae3795fab
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-03 14:36:07 +0330

    temp: call function with wrong object property

--- objects-unions/objects.ts
+++ objects-unions/objects.ts
@@ -2,3 +2,5 @@ function printCoord(pt: { x: number; y: number }) {
   console.log("The coordinate's x is: ", pt.x);
   console.log("The coordinate's y is: ", pt.y);
 }
+
+printCoord({ x: 5, y: 6, z: 7 });
