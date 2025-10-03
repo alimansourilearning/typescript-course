231490d040cba42b1929761dea52c55e1b83b853
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-03 14:45:20 +0330

    feat: define and call function for optional properties

--- objects-unions/objects.ts
+++ objects-unions/objects.ts
@@ -3,4 +3,9 @@ function printCoord(pt: { x: number; y: number }) {
   console.log("The coordinate's y is: ", pt.y);
 }
 
-printCoord({ x: 5, y: 6, z: 7 });
+// printCoord({ x: 5, y: 6, z: 7 });
+
+function printName(name: { firstname: string; lastname?: string }) {}
+
+printName({ firstname: "ali" });
+printName({ firstname: "ahmad", lastname: "ahmadi" });
