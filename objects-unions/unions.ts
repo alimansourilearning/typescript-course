c4fa6b9e72a8084330978026a06b1b0d3e3b0d36
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-03 16:45:50 +0330

    temp: use some members methods for all

--- objects-unions/unions.ts
+++ objects-unions/unions.ts
@@ -1,7 +1,7 @@
 function printId(id: string | number) {
-  console.log("Your id is: ", id);
+  console.log("Your id is: ", id.toUpperCase());
 }
 
 printId(12334);
 printId("dkfjsdjflkdsjfl;djlfsdj;fsd");
-printId({ id: "lksdfjdfjskldkfjlsdjflds" });
+// printId({ id: "lksdfjdfjskldkfjlsdjflds" });
