ea71eec86269942624e34717a2069ad22752a6ff
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-03 16:39:37 +0330

    temp: call union function  with wrong or ok argument

--- objects-unions/unions.ts
+++ objects-unions/unions.ts
@@ -1,4 +1,7 @@
-function printId(id:
-    | string | number) {
+function printId(id: string | number) {
   console.log("Your id is: ", id);
 }
+
+printId(12334);
+printId("dkfjsdjflkdsjfl;djlfsdj;fsd");
+printId({ id: "lksdfjdfjskldkfjlsdjflds" });
