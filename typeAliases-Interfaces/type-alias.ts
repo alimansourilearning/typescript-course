b4f67f9bc3295eaeea7e006ee222ac4268713d77
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-04 01:51:40 +0330

    feat: add function for type alias for unions

--- typeAliases-Interfaces/type-alias.ts
+++ typeAliases-Interfaces/type-alias.ts
@@ -4,3 +4,8 @@ function showCoordinate(pt: Point) {
   console.log("The coordinate's x is:", pt.x);
   console.log("The coordinate's y is:", pt.y);
 }
+
+type Id = string | number;
+function showId(id: Id) {
+  console.log("Your id is:", id);
+}
