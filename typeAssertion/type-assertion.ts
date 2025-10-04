b766a07f3a14b23dfa4efa2b9d00e2a64211d0e9
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-04 15:58:00 +0330

    temp: wrong as any as t type assertion

--- typeAssertion/type-assertion.ts
+++ typeAssertion/type-assertion.ts
@@ -1,4 +1,7 @@
 const myElement = <HTMLButtonElement>document.getElementById("abc123");
 myElement.disabled = true;
 
-let x = "hello" as number
+// let x = "hello" as number
+
+let a = "sk;dlaskdlsk;ldskad;s"
+let b = a as any as number;
