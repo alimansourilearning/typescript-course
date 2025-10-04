f8945cd0cf8a83ec435f168fd7b85b810dc81881
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-04 13:51:17 +0330

    feat: example for interface extend

--- typeAliases-Interfaces/interface.ts
+++ typeAliases-Interfaces/interface.ts
@@ -7,3 +7,15 @@ function showNewCoordinate(pt: NewPoint) {
   console.log("The coordinate's x is:", pt.x);
   console.log("The coordinate's y is:", pt.y);
 }
+
+interface NewAnimal {
+  name: string;
+}
+interface NewBear extends NewAnimal {
+  honey: boolean;
+}
+
+const newBear: NewBear = {
+  name: "Winnie",
+  honey: true,
+};
