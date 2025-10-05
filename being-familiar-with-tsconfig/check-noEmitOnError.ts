890cd10cc239cce76ebebdd9fa7e02afa7794509
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-05 21:30:36 +0330

    temp: create another error for noEmit in terminal for tsc with file name

--- being-familiar-with-tsconfig/check-noEmitOnError.ts
+++ being-familiar-with-tsconfig/check-noEmitOnError.ts
@@ -1,3 +1,4 @@
 let greetingMessage: number = "Hello";
 let newMessage: boolean = "Thanks";
 let newGreetingMessage: number = "Hello, thanks";
+let anotherGreetingMessage: boolean = "Hello, thank you so much";
