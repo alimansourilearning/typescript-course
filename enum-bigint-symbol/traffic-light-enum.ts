633a2a935b2df146ec773e965a7f9e7905c237c4
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-09 16:05:12 +0330

    feat: use enum in a function

--- enum-bigint-symbol/traffic-light-enum.ts
+++ enum-bigint-symbol/traffic-light-enum.ts
@@ -3,3 +3,13 @@ enum TrafficLightStatus {
   GREEN,
   YELLOW,
 }
+
+function handleTrafficLightStatus(status: TrafficLightStatus) {
+  if (status === TrafficLightStatus.RED) {
+    console.log("STOP!");
+  } else if (status === TrafficLightStatus.GREEN) {
+    console.log("GO");
+  } else {
+    console.log("WAIT!");
+  }
+}
