110f373d9129a435a962925753349aa8fefac26e
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-09 14:38:21 +0330

    feat: define function to handle traffic light status

--- enum-bigint-symbol/traffic-light-without-enum.ts
+++ enum-bigint-symbol/traffic-light-without-enum.ts
@@ -1,3 +1,13 @@
 const RED_STATUS = 0;
 const GREEN_STATUS = 1;
 const YELLOW_STATUS = 2;
+
+function showTrafficLightStatus(status: number) {
+  if (status === RED_STATUS) {
+    console.log("STOP!");
+  } else if (status === GREEN_STATUS) {
+    console.log("GO");
+  } else {
+    console.log("WAIT!");
+  }
+}
