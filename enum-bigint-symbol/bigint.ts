51de8e7a494f0969c72c07bdd8c86b92779e802d
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-10 16:47:24 +0330

    feat: BigInt function the second way to define bigint

--- enum-bigint-symbol/bigint.ts
+++ enum-bigint-symbol/bigint.ts
@@ -1,3 +1,4 @@
 let firstNumber = 999999999999999; // 15 digits
-let secondNumber = 99999999999999999999n; // 20 digits
+// let secondNumber = 99999999999999999999n; // 20 digits
+let secondNumber = BigInt("99999999999999999999"); // 20 digits
 console.log(firstNumber, secondNumber);
