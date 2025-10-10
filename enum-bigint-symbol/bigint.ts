25f9de5d96540817c8b24f70d3cd09aa0550cfeb
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-10 19:33:28 +0330

    temp: number plus bigint

--- enum-bigint-symbol/bigint.ts
+++ enum-bigint-symbol/bigint.ts
@@ -1,5 +1,6 @@
 let firstNumber = 999999999999999; // 15 digits
 // let secondNumber = 99999999999999999999n; // 20 digits
 let secondNumber = BigInt("99999999999999999999"); // 20 digits
+let thirdNumber = firstNumber + secondNumber;
 // console.log(firstNumber, secondNumber);
 console.log(typeof secondNumber);
