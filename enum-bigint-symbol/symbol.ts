fd67a4735101d38d31665bb586460da5dcd57832
alimansourilearning <ali.mansourilearning@gmail.com>
2025-10-13 21:56:17 +0330

    feat: add two comparison between both consts & both lets

--- enum-bigint-symbol/symbol.ts
+++ enum-bigint-symbol/symbol.ts
@@ -1,8 +1,12 @@
 let firstValue = Symbol("value");
 let thirdValue = Symbol();
+if (firstValue === thirdValue) {
+}
 
 const secondValue = Symbol();
 const fourthValue = Symbol();
+if (secondValue === fourthValue) {
+}
 
 // console.log(firstValue);
 
